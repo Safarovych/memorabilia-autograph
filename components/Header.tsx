9db28e33c03c884bc5890fea645c942d1aa34f67
Safarovych <atlant176@icloud.com>
2026-09-20 20:29:40 +0200

    design: refresh header for premium light theme

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,29 +2,20 @@ import Link from 'next/link';
 import { BagIcon, HeartIcon, SearchIcon, UserIcon } from './Icon';
 
 export default function Header() {
-  return (
-    <>
-      <div className="topline">
-        <div>EN · EUR</div>
-        <div>Worldwide shipping • Collector support</div>
-      </div>
-      <header className="header">
-        <Link className="logoWrap" href="/">
-          <div className="logoMark">MA</div>
-          <div className="logoText">MEMORABILIA AUTOGRAPH<span>FOOTBALL • SIGNED • COLLECTIBLE</span></div>
-        </Link>
-        <nav>
-          {[['SHOP', '/shop'], ['AUCTIONS', '/auctions'], ['SIGNED', '/shop?category=Signed'], ['CLUBS', '/shop'], ['PLAYERS', '/shop'], ['LEGENDS', '/auctions'], ['ABOUT', '/#about']].map(([label, href]) => (
-            <Link key={label} href={href}>{label}</Link>
-          ))}
-        </nav>
-        <div className="actions">
-          <Link href="/shop" aria-label="Search"><SearchIcon /></Link>
-          <Link href="/login" aria-label="Account"><UserIcon /></Link>
-          <Link href="/shop" aria-label="Wishlist"><HeartIcon /></Link>
-          <Link href="/cart" aria-label="Cart"><BagIcon /></Link>
-        </div>
-      </header>
-    </>
-  );
+  return <header className="premiumHeader">
+    <Link className="premiumLogo" href="/">
+      <span className="premiumLogoMark">MA</span>
+      <span><strong>MEMORABILIA AUTOGRAPH</strong><small>AUTHENTIC SPORTS MEMORABILIA</small></span>
+    </Link>
+    <nav>
+      {[['Home','/'],['Shop','/shop'],['Auctions','/auctions'],['About','/#about'],['Authenticity','/shop?category=Signed'],['Charity','/about'],['Contact','/#contact']].map(([label,href])=><Link key={label} href={href}>{label}</Link>)}
+    </nav>
+    <div className="premiumActions">
+      <Link href="/shop" aria-label="Search"><SearchIcon/></Link>
+      <Link href="/login" aria-label="Account"><UserIcon/></Link>
+      <Link href="/shop" aria-label="Wishlist"><HeartIcon/></Link>
+      <Link href="/cart" aria-label="Cart" className="cartAction"><BagIcon/><b>0</b></Link>
+      <span className="language">🇬🇧 EN⌄</span>
+    </div>
+  </header>;
 }
