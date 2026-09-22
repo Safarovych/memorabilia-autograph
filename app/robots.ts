7d8d7387f5fa32f82a8fb4a69acb3f20b12ec5d8
Safarovych <atlant176@gmail.com>
2026-09-22 21:49:07 +0200

    SEO: configure robots rules

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,11 @@
 import type { MetadataRoute } from 'next';
+import { siteUrl } from './layout';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: '*', allow: '/' }],
-    sitemap: 'https://memorabilia-autograph.com/sitemap.xml',
+    rules: [
+      { userAgent: '*', allow: '/', disallow: ['/admin/', '/api/', '/login', '/register', '/cart'] },
+    ],
+    sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
