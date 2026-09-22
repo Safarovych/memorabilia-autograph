3814b3f5916f3365b4215fa1cf34091bff057509
Safarovych <atlant176@icloud.com>
2026-09-22 14:31:39 +0200

    Clean unused product page import

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import PageShell from '../../../components/PageShell';
 import LanguageText from '../../../components/LanguageText';
